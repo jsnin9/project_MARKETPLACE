93725172997ae6fb3161cc54d73ea9a29e3a2891
jsnin9 <103622164+jsnin9@users.noreply.github.com>
2022-04-14 07:22:24 +0200

    Update MarketPlace.java

--- MARKETPLACE/src/MarketPlace.java
+++ MARKETPLACE/src/MarketPlace.java
@@ -25,6 +25,57 @@ public class MarketPlace {
 	public void resilierContrat(Contrat c) {
 		c.setEtat(false);
 	}
+	
+	void publierProduit() throws NosExceptions{
+					
+		try {
+
+			Scanner sc = new Scanner(System.in);
+					
+			super.verif();  /*vérification de l'état de connexion du vendeur*/
+			
+			
+			System.out.println("\nInitialisation de la publication");
+
+			
+			System.out.println("\nEntrez le nom du produit");
+			String nom = sc.nextLine();
+			NosExceptions.verifChamp(nom);
+
+				
+			System.out.println("Entrez le prix du produit");
+			String prop  = sc.nextLine();
+			NosExceptions.verifChamp(prop);
+			float prix = Float.parseFloat(prop);
+
+			
+			System.out.println("Entrez la catégorie du produit");
+			String categorie = sc.nextLine();
+			NosExceptions.verifChamp(categorie);
+
+			
+			System.out.println("Entrez la déscription du produit");
+			String description = sc.nextLine();
+			NosExceptions.verifChamp(description);
+
+			
+			System.out.println("Entrez la durée de livraison du produit");
+			String estimation = sc.nextLine(); 
+			NosExceptions.verifChamp(estimation);
+
+			
+			Produit p = new Produit(nom, prix, categorie, estimation, null, description); /*création du produit*/
+			Produit.addProduit(p);    /*publication du produit*/
+			
+			System.out.println("\nPublication réussi avec succès !");
+
+
+		} catch(NosExceptions e) {
+			
+			System.out.println(e.getMessage());  /* on informe qu'il faut se connecter avant de publier un produit*/
+			
+		}
+	}
 }
 	
 	
